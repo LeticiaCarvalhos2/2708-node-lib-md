fc3b1909646f3952f0883c600f42f696f8991795
LeticiaCarvalhos2 <leticia.carvalhos2s2@gmail.com>
2023-09-21 10:10:06 -0300

    finalizacao curso biblioteca

--- src/index.js
+++ src/index.js
@@ -1,40 +1,28 @@
-// const chalk = require('chalk'); está entrando em desuso
-import fs, { readFile } from 'fs';
+import fs from 'fs';
 import chalk from 'chalk';
-import { error } from 'console';
-
-// console.log(chalk.blue('olá mundo!')); exemplo chalk
-
-function trataErro(erro){
-    console.log(erro);
-    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
-}
-
-//async/await
-async function pegaArquivo(caminhoDoArquivo){
-    try{
-        const encoding = 'utf-8';
-        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        return extraiLinks(texto);
-    }catch(erro){
-        trataErro(erro)
-    }
-    
-}
 
 function extraiLinks(texto) {
-    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-    const capturas = [...texto.matchAll(regex)];
-    const resultados = capturas.map(captura => ({[captura[1]] : [captura[2]]}));
-    return resultados.length !== 0 ? resultados : 'Não há links no arquivo';
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+  return resultados.length !== 0 ? resultados : 'não há links no arquivo';
 }
 
+function trataErro(erro) {
+  console.log(erro);
+  throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+// async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = 'utf-8';
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    return extraiLinks(texto);
+  } catch (erro) {
+    trataErro(erro)
+  }
+}
 
 export default pegaArquivo;
-
-// pegaArquivo('./arquivos/');
-
-
-// \[[^[\]]*?\]
-
-// \(https?:\/\/[^\s?#.].[^\s]*\)
